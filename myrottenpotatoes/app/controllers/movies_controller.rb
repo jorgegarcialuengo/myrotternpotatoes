023947a4610b4eb2fd5fdd63643031f03e340a6b
Jorge García-Luengo <jorgegarcialuengo@gmail.com>
2015-01-21 12:51:00 -0400

    create movie page

--- myrottenpotatoes/app/controllers/movies_controller.rb
+++ myrottenpotatoes/app/controllers/movies_controller.rb
@@ -2,12 +2,17 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+
   end
 
   def show
     id = params[:id] # retreave the ID from URI route
     @movie = Movie.find(id) # lookup movie by unique ID
     # will render app/views/movies/show.html.haml by default
+  end
+
+  def new
+    # default: render 'new' template
   end
 end
 
