08381c9582b319e0dcfa26e643d78389d318aa71
Jorge García-Luengo <jorgegarcialuengo@gmail.com>
2015-01-30 17:06:46 -0500

    twitter auth working

--- myrottenpotatoes/app/controllers/application_controller.rb
+++ myrottenpotatoes/app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_current_user
+  protected # prents method form being invoqued by a route
+  def set_current_user
+    # we exploit the fact that find_by_id(nil) returns nil
+    @current_user ||= Moviegoer.find_by_id(session[:user_id])
+
+    # redirect_to login_path and return unless @current_user # and is like && but with lower presence, unless is computed before
+  end
+
 end
